8ba211a1c88f906c0fa7429236ce413dca306464
Leturgone <finekon221@gmail.com>
2025-02-21 17:48:24 +0300

    Создана функция RegScreen

--- app/src/main/java/com/example/employeetaskreg/view/logRegScreen/RegScreen.kt
+++ app/src/main/java/com/example/employeetaskreg/view/logRegScreen/RegScreen.kt
@@ -1,2 +1,11 @@
 package com.example.employeetaskreg.view.logRegScreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegScreen(){
+    Column {
+
+    }
+}
