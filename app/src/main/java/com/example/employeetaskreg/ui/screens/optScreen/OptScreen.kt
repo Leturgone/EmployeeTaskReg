432523259c4d33dc90176f9a50c5d14339a97d78
Leturgone <finekon221@gmail.com>
2025-02-26 00:53:18 +0300

    Сделана навигация OptScreen

--- app/src/main/java/com/example/employeetaskreg/ui/screens/optScreen/OptScreen.kt
+++ app/src/main/java/com/example/employeetaskreg/ui/screens/optScreen/OptScreen.kt
@@ -8,14 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,18 +19,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.employeetaskreg.R
 
 @Composable
-@Preview
-fun OptScreen(){
+fun OptScreen(navController: NavHostController) {
     var switchState by remember { mutableStateOf(true) }
     Column {
         Box(modifier = Modifier.fillMaxWidth()){
@@ -61,7 +53,10 @@ fun OptScreen(){
                 modifier = Modifier
                     .align(Alignment.BottomStart)
                     .padding(start = 16.dp, top = 16.dp)
-                    .clickable { }
+                    .clickable {
+                        navController.popBackStack()
+                        navController.popBackStack()
+                        navController.navigate("reg") }
             )
         }
         Box(modifier = Modifier
