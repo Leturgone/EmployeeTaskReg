85fa0c99f0c8c0359c23681ec5418c2c157381d6
Leturgone <finekon221@gmail.com>
2025-03-27 04:19:33 +0300

    Сделан DI

---
+++ app/src/main/java/com/example/employeetaskreg/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.example.employeetaskreg.di
+
+import com.example.employeetaskreg.data.api.EmployeeTaskRegApi
+import com.example.employeetaskreg.data.repsitory.EmployeeTaskRegRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideMarvelComicApi():EmployeeTaskRegApi{
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+
+        val client = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS) // Увеличение времени ожидания соединения
+            .readTimeout(60, TimeUnit.SECONDS)    // Увеличение времени ожидания чтения данных
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(logging)
+            .build()
+        return Retrofit
+            .Builder().baseUrl("https://gateway.marvel.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(EmployeeTaskRegApi::class.java)
+    }
+    @Provides
+    @Singleton
+    fun provideRemoteRepository(api: EmployeeTaskRegApi):EmployeeTaskRegRepositoryImpl{
+        return EmployeeTaskRegRepositoryImpl(api)
+    }
+}
