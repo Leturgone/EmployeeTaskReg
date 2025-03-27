bf1cbae44565d58d17dfde3b2ba5957dc3a54133
Leturgone <finekon221@gmail.com>
2025-03-27 04:20:03 +0300

    Создан репозиторий для API

---
+++ app/src/main/java/com/example/employeetaskreg/data/repsitory/EmployeeTaskRegRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.example.employeetaskreg.data.repsitory
+
+import com.example.employeetaskreg.data.api.EmployeeTaskRegApi
+
+class EmployeeTaskRegRepositoryImpl(api: EmployeeTaskRegApi) {
+}
