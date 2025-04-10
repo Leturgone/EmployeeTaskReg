dc1b9fce9173e6134e4df17c95a2654e1d49be7d
Leturgone <finekon221@gmail.com>
2025-04-10 21:24:53 +0300

    Добавлены роуты

--- app/src/main/java/com/example/employeetaskreg/data/api/EmployeeTaskRegApi.kt
+++ app/src/main/java/com/example/employeetaskreg/data/api/EmployeeTaskRegApi.kt
@@ -1,9 +1,59 @@
 package com.example.employeetaskreg.data.api
 
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface EmployeeTaskRegApi {
 
+    @GET("/profile")
+    suspend fun getProfile()
+    @POST("/profile/addTask")
+    suspend fun addTask()
+
+    @POST("/profile/addReport")
+    suspend fun addReport()
+
     @GET("/profile/myEmployees")
     suspend fun getEmployees()
+
+    @GET("profile/myEmployees/{empName}")
+    suspend fun getEmployeeByName()
+
+    @GET("profile/myEmployees/employee/{employeeId}")
+    suspend fun getEmployeeById()
+
+    @GET("/profile/myTasks")
+    suspend fun getTasks()
+
+    @GET("/profile/myReports")
+    suspend fun getReports()
+
+    @GET("/profile/myTaskCount")
+    suspend fun getTaskCount()
+
+    @GET("/getReport/{reportId}")
+    suspend fun getReportById()
+
+    @GET("/getReport/{reportId}/download")
+    suspend fun downloadReportById()
+
+    @PATCH("/markReport/{reportId}/{status}")
+    suspend fun markReportById()
+
+    @GET("/getTask/{taskId}")
+    suspend fun getTaskById()
+
+    @GET("/getTask/{taskId}/download")
+    suspend fun downloadTaskById()
+
+    @POST("/users/register")
+    suspend fun register()
+
+    @POST("/users/login")
+    suspend fun login()
+
+
+
 }
