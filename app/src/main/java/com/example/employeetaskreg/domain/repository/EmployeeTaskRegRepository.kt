d6e278a9e410a2c3c24c2b29ba9932d006623c90
Leturgone <finekon221@gmail.com>
2025-04-20 17:04:50 +0300

    Созданы функции рега, лога и профиля

--- app/src/main/java/com/example/employeetaskreg/domain/repository/EmployeeTaskRegRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/EmployeeTaskRegRepository.kt
@@ -1,5 +1,11 @@
 package com.example.employeetaskreg.domain.repository
 
-interface EmployeeTaskRegRepository {
+import com.example.employeetaskreg.domain.model.CompanyWorker
 
+interface EmployeeTaskRegRepository {
+    suspend fun login(login:String, password:String):Result<String>
+
+    suspend fun register(login:String, password:String, name: String, dirName:String):Result<String>
+
+    suspend fun getProfile():CompanyWorker
 }
