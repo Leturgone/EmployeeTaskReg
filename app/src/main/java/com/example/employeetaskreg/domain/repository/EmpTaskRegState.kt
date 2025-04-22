6bd4bd914be9417411830b960700b1faddfe978e
Leturgone <finekon221@gmail.com>
2025-04-22 08:56:01 +0300

    Добавлено состояние

--- app/src/main/java/com/example/employeetaskreg/domain/repository/EmpTaskRegState.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/EmpTaskRegState.kt
@@ -3,6 +3,8 @@ package com.example.employeetaskreg.domain.repository
 sealed class EmpTaskRegState<out R>  {
     data class Success<out R>(val result: R): EmpTaskRegState<R>()
 
+    data object Waiting : EmpTaskRegState<Nothing>()
+
     data class Failure(val exception: Exception) :EmpTaskRegState<Nothing>()
 
     data object Loading : EmpTaskRegState<Nothing>()
