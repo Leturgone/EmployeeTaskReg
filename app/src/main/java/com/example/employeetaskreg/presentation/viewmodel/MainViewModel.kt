6645ee88cbfb0bddfc0746ccf9a17f6892ba8d76
Leturgone <finekon221@gmail.com>
2025-04-22 14:54:01 +0300

    Добавлен метод получения профиля

--- app/src/main/java/com/example/employeetaskreg/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/employeetaskreg/presentation/viewmodel/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.employeetaskreg.domain.model.CompanyWorker
 import com.example.employeetaskreg.domain.repository.EmpTaskRegState
 import com.example.employeetaskreg.domain.repository.EmployeeTaskRegRepository
 import com.example.employeetaskreg.presentation.ui.components.Employee
@@ -12,6 +13,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,11 +25,13 @@ class MainViewModel @Inject constructor(private val employeeTaskRegRepository: E
 
     private val _regFlow = MutableStateFlow<EmpTaskRegState<String>>(EmpTaskRegState.Waiting)
 
-    val regFlow: StateFlow<EmpTaskRegState<String>> = _loginFlow
+    val regFlow: StateFlow<EmpTaskRegState<String>> = _regFlow
 
+    private val _profileFlow = MutableStateFlow<EmpTaskRegState<CompanyWorker>>(EmpTaskRegState.Waiting)
 
+    val profileFlow: StateFlow<EmpTaskRegState<CompanyWorker>> = _profileFlow
 
-     private val _userRole = MutableLiveData<String>("1")
+    private val _userRole = MutableLiveData<String>("1")
     val userRole: LiveData<String> = _userRole
 
     private val _empList  = MutableLiveData<List<Employee>>()
@@ -36,14 +40,26 @@ class MainViewModel @Inject constructor(private val employeeTaskRegRepository: E
         _userRole.value = role
     }
 
-    fun register(login: String, password: String, name: String, dirName: String) = viewModelScope.launch(Dispatchers.IO){
+    fun register(login: String, password: String, name: String, dirName: String) = viewModelScope.launch{
        _regFlow.value = EmpTaskRegState.Loading
-        val result = employeeTaskRegRepository.register(login, password, name, dirName)
+        val result = withContext(Dispatchers.IO){
+            employeeTaskRegRepository.register(login, password, name, dirName)
+        }
         _regFlow.value = result
     }
-    fun login(login: String, password: String) = viewModelScope.launch(Dispatchers.IO){
+    fun login(login: String, password: String) = viewModelScope.launch{
         _loginFlow.value = EmpTaskRegState.Loading
-        val result = employeeTaskRegRepository.login(login, password)
+        val result = withContext(Dispatchers.IO){
+            employeeTaskRegRepository.login(login, password)
+        }
         _loginFlow.value = result
+    }
+
+    fun getProfile() = viewModelScope.launch {
+        _profileFlow.value = EmpTaskRegState.Loading
+        val result = withContext(Dispatchers.IO){
+            employeeTaskRegRepository.getProfile()
+        }
+        _profileFlow.value = result
     }
 }
