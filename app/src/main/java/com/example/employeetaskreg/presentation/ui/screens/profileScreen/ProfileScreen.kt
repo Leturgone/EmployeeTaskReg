44096ea5e58032ce606705fbe2788eccda71b5bf
Leturgone <finekon221@gmail.com>
2025-04-23 21:36:14 +0300

    Сделана загрузка профиля

--- app/src/main/java/com/example/employeetaskreg/presentation/ui/screens/profileScreen/ProfileScreen.kt
+++ app/src/main/java/com/example/employeetaskreg/presentation/ui/screens/profileScreen/ProfileScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -28,15 +29,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.employeetaskreg.R
 import com.example.employeetaskreg.domain.model.CompanyWorker
-import com.example.employeetaskreg.domain.model.Director
-import com.example.employeetaskreg.domain.model.Employee
+import com.example.employeetaskreg.domain.model.CompanyWorkerInterface
 import com.example.employeetaskreg.domain.repository.EmpTaskRegState
 import com.example.employeetaskreg.presentation.ui.screens.CustomToastMessage
 import com.example.employeetaskreg.presentation.viewmodel.MainViewModel
@@ -46,10 +45,16 @@ import com.example.employeetaskreg.presentation.viewmodel.MainViewModel
 fun ProfileScreen(navController: NavHostController,role:String, viewModel: MainViewModel = hiltViewModel()) {
 
     val profileState = viewModel.profileFlow.collectAsState()
+    val dirNameState = viewModel.dirNameFlow.collectAsState()
+    val taskCountState = viewModel.taskCountFlow.collectAsState()
+
+    var dirName by remember { mutableStateOf("") }
+    var taskCount by remember { mutableIntStateOf(0) }
     var showToast by remember { mutableStateOf(false) }
     var errorMessage by remember { mutableStateOf("") }
     LaunchedEffect(Unit){
         viewModel.getProfile()
+        viewModel.getTaskCount()
     }
     CustomToastMessage(
         message = errorMessage,
@@ -63,7 +68,7 @@ fun ProfileScreen(navController: NavHostController,role:String, viewModel: MainV
         }
         EmpTaskRegState.Loading -> CircularProgressIndicator()
         is EmpTaskRegState.Success -> {
-            val profile = (profileState.value as EmpTaskRegState.Success<CompanyWorker>).result
+            val profile = (profileState.value as EmpTaskRegState.Success<CompanyWorkerInterface>).result
             Column {
                 Box(modifier = Modifier.fillMaxWidth()){
                     Text(
@@ -118,7 +123,7 @@ fun ProfileScreen(navController: NavHostController,role:String, viewModel: MainV
                     }
                 }
                 when(profile){
-                    is Director->{
+                    is CompanyWorker.Director->{
                         Box(modifier = Modifier
                             .fillMaxWidth()
                             .height(100.dp)){
@@ -133,7 +138,7 @@ fun ProfileScreen(navController: NavHostController,role:String, viewModel: MainV
                             )
                         }
                     }
-                    is Employee->{
+                    is CompanyWorker.Employee->{
                         Box(modifier = Modifier
                             .fillMaxWidth()
                             .height(100.dp)){
@@ -150,8 +155,20 @@ fun ProfileScreen(navController: NavHostController,role:String, viewModel: MainV
                         Box(modifier = Modifier
                             .fillMaxWidth()
                             .height(100.dp)){
+
+                            LaunchedEffect(Unit){
+                                profile.directorId?.let { viewModel.getDirById(it) }
+                            }
+
+                            dirName = when(dirNameState.value){
+                                is EmpTaskRegState.Failure -> ""
+                                EmpTaskRegState.Loading -> "..."
+                                is EmpTaskRegState.Success -> (dirNameState.value as EmpTaskRegState.Success<String>).result
+                                EmpTaskRegState.Waiting -> ""
+                            }
+
                             Text(
-                                text = "${stringResource(id = R.string.director)}: ",
+                                text = "${stringResource(id = R.string.director)}: $dirName ",
                                 style = MaterialTheme.typography.headlineSmall,
                                 fontWeight = FontWeight.Normal,
                                 fontSize = 25.sp,
@@ -163,12 +180,17 @@ fun ProfileScreen(navController: NavHostController,role:String, viewModel: MainV
                     }
                 }
 
-
                 Box(modifier = Modifier
                     .fillMaxWidth()
                     .height(100.dp)){
+                    taskCount = when(taskCountState.value){
+                        is EmpTaskRegState.Failure -> 0
+                        EmpTaskRegState.Loading -> 0
+                        is EmpTaskRegState.Success -> (taskCountState.value as EmpTaskRegState.Success<Int>).result
+                        EmpTaskRegState.Waiting -> 0
+                    }
                     Text(
-                        text = "${stringResource(id = R.string.tasks_solved)} 10",
+                        text = "${stringResource(id = R.string.tasks_solved)}: $taskCount",
                         style = MaterialTheme.typography.headlineSmall,
                         fontWeight = FontWeight.Normal,
                         fontSize = 25.sp,
