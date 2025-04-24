18b06bd151d3235efe8c7f5008fb940017719a21
Leturgone <finekon221@gmail.com>
2025-04-24 23:50:39 +0300

    Добавлен метод получения списка заданий

--- app/src/main/java/com/example/employeetaskreg/domain/repository/EmployeeTaskRegRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/EmployeeTaskRegRepository.kt
@@ -1,6 +1,7 @@
 package com.example.employeetaskreg.domain.repository
 
 import com.example.employeetaskreg.domain.model.CompanyWorker
+import com.example.employeetaskreg.domain.model.Task
 
 interface EmployeeTaskRegRepository {
 
@@ -16,4 +17,6 @@ interface EmployeeTaskRegRepository {
     suspend fun getDirectorNameById(id: Int):EmpTaskRegState<String>
 
     suspend fun getTaskCount():EmpTaskRegState<Int>
+
+    suspend fun getTaskList():EmpTaskRegState<List<Task>>
 }
