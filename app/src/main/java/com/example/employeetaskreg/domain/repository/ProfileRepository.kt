69145afb432a481d7fddd2f69012355154e7b404
Leturgone <finekon221@gmail.com>
2025-05-13 11:39:09 +0300

    added result to ProfileRepository

--- app/src/main/java/com/example/employeetaskreg/domain/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/ProfileRepository.kt
@@ -3,5 +3,5 @@ package com.example.employeetaskreg.domain.repository
 import com.example.employeetaskreg.domain.model.CompanyWorker
 
 interface ProfileRepository {
-    suspend fun getProfile():EmpTaskRegState<CompanyWorker>
+    suspend fun getProfile(authToken:String):Result<CompanyWorker>
 }
