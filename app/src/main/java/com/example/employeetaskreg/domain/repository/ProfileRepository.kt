a1817e0dc81e9eb455f6ad29abe113548f217ab3
Leturgone <finekon221@gmail.com>
2025-05-13 12:02:27 +0300

    added result to TaskRepository

--- app/src/main/java/com/example/employeetaskreg/domain/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/ProfileRepository.kt
@@ -3,5 +3,6 @@ package com.example.employeetaskreg.domain.repository
 import com.example.employeetaskreg.domain.model.CompanyWorker
 
 interface ProfileRepository {
+
     suspend fun getProfile(authToken:String):Result<CompanyWorker>
 }
--- app/src/main/java/com/example/employeetaskreg/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/TaskRepository.kt
@@ -3,10 +3,11 @@ package com.example.employeetaskreg.domain.repository
 import com.example.employeetaskreg.domain.model.Task
 
 interface TaskRepository {
-    suspend fun getTaskCount():Result<Int>
 
-    suspend fun getTaskList():Result<List<Task>>
+    suspend fun getTaskCount(authToken:String):Result<Int>
 
-    suspend fun getEmployeeTaskCount(id:Int):EmpTaskRegState<Int>
+    suspend fun getTaskList(authToken:String):Result<List<Task>>
+
+    suspend fun getEmployeeTaskCount(id:Int,authToken:String):Result<Int>
 
 }
