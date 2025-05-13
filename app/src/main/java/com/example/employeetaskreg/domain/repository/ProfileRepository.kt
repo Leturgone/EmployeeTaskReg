20667dd07be7603fe6e90ab53b78051049eda952
Leturgone <finekon221@gmail.com>
2025-05-13 10:56:12 +0300

    profile methods rebased to profileRepository

---
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/ProfileRepository.kt
@@ -0,0 +1,7 @@
+package com.example.employeetaskreg.domain.repository
+
+import com.example.employeetaskreg.domain.model.CompanyWorker
+
+interface ProfileRepository {
+    suspend fun getProfile():EmpTaskRegState<CompanyWorker>
+}
