4fc3914fdb49d949fc42bb0e00880395fd5374ce
Leturgone <finekon221@gmail.com>
2025-05-14 20:16:32 +0300

    added ProfileScreen viewmodel

--- app/src/main/java/com/example/employeetaskreg/domain/repository/EmployeeTaskRegRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/EmployeeTaskRegRepository.kt
@@ -6,12 +6,12 @@ import com.example.employeetaskreg.domain.model.Task
 
 interface EmployeeTaskRegRepository {
 
-//    suspend fun getTokenFromDataStorage():String
-//    suspend fun login(login:String, password:String):EmpTaskRegState<String>
+    suspend fun getTokenFromDataStorage():String
+    suspend fun login(login:String, password:String):EmpTaskRegState<String>
 //
-//    suspend fun register(login:String, password:String, name: String, dirName:String):EmpTaskRegState<String>
+    suspend fun register(login:String, password:String, name: String, dirName:String):EmpTaskRegState<String>
 //
-//    suspend fun logout()
+    suspend fun logout()
 
     //suspend fun getProfile():EmpTaskRegState<CompanyWorker>
 
