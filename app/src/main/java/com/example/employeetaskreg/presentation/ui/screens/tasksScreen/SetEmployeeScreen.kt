8ee5660a214fbb00056f2eef4ae8014b40a058b2
Leturgone <finekon221@gmail.com>
2025-05-15 02:02:20 +0300

    added toast

--- app/src/main/java/com/example/employeetaskreg/presentation/ui/screens/tasksScreen/SetEmployeeScreen.kt
+++ app/src/main/java/com/example/employeetaskreg/presentation/ui/screens/tasksScreen/SetEmployeeScreen.kt
@@ -7,21 +7,44 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.employeetaskreg.R
+import com.example.employeetaskreg.domain.model.CompanyWorker
+import com.example.employeetaskreg.domain.repository.EmpTaskRegState
+import com.example.employeetaskreg.presentation.ui.screens.CustomToastMessage
 import com.example.employeetaskreg.presentation.ui.screens.employeeScreen.EmployeeCard
 import com.example.employeetaskreg.presentation.ui.screens.employeeScreen.SearchSec
+import com.example.employeetaskreg.presentation.viewmodel.MainViewModel
 
 @Composable
-fun SetEmployeeScreen(navController: NavHostController) {
+fun SetEmployeeScreen(navController: NavHostController, viewModel: MainViewModel = hiltViewModel()) {
+
+    val employeeListState = viewModel.employeesListFlow.collectAsState()
+
+    var showToast by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+    var employeeList by remember { mutableStateOf(emptyList<CompanyWorker.Employee>()) }
+
+
+    LaunchedEffect(Unit){
+        viewModel.getEmployeesList()
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,16 +60,39 @@ fun SetEmployeeScreen(navController: NavHostController) {
 
         SearchSec()
 
+        CustomToastMessage(
+            message = errorMessage,
+            isVisible = showToast,
+            onDismiss = { showToast = false },
+        )
+
+        when(employeeListState.value){
+            is EmpTaskRegState.Failure -> LaunchedEffect(employeeListState.value) {
+                showToast = true
+                errorMessage =
+                    (employeeListState.value as EmpTaskRegState.Failure).exception.toString()
+            }
+            EmpTaskRegState.Loading -> CircularProgressIndicator()
+            is EmpTaskRegState.Success -> employeeList = (employeeListState.value as EmpTaskRegState.Success<List<CompanyWorker.Employee>>).result
+            EmpTaskRegState.Waiting -> null
+        }
+
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(3) {
+            items(employeeList.size) {
+                val employee = employeeList[it]
                 Spacer(modifier = Modifier.height(30.dp))
-                EmployeeCard(name = "Иванов И.И.",true){
+                EmployeeCard(
+                    employeeName = employee.name,
+                    employeeId = employee.id,
+                    setListItem = true
+                ){
+                    viewModel.selectEmployeeForTask(employee)
                     navController.popBackStack()
                     navController.popBackStack()
-                    navController.navigate("new_task/${"Иванов И.И."}")
+                    navController.navigate("new_task/${employee.name}/${employee.id}")
                 }
 
             }
