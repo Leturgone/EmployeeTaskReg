d17d9f793150ce671052c72823e7ae26b7204151
Leturgone <finekon221@gmail.com>
2025-05-14 20:16:50 +0300

    added RespScreen viewmodel

---
+++ app/src/main/java/com/example/employeetaskreg/presentation/viewmodel/RespScreenViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.employeetaskreg.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.employeetaskreg.domain.model.Report
+import com.example.employeetaskreg.domain.repository.AuthRepository
+import com.example.employeetaskreg.domain.repository.EmpTaskRegState
+import com.example.employeetaskreg.domain.repository.ReportRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class RespScreenViewModel @Inject constructor(
+    private val reportRepository: ReportRepository,private val authRepository: AuthRepository): ViewModel() {
+
+    private val _reportListFlow = MutableStateFlow<EmpTaskRegState<List<Report>>>(EmpTaskRegState.Waiting)
+
+    val reportListFlow: StateFlow<EmpTaskRegState<List<Report>>> = _reportListFlow
+
+    fun getReportList() = viewModelScope.launch {
+        _reportListFlow.value = EmpTaskRegState.Loading
+
+        val authResult = withContext(Dispatchers.IO){
+            authRepository.getTokenFromDataStorage()
+        }
+
+        authResult.onSuccess {token ->
+            val result = withContext(Dispatchers.IO){
+                reportRepository.getReportList(token)
+            }
+            result.onSuccess {
+                _reportListFlow.value = EmpTaskRegState.Success(it)
+            }.onFailure {
+                _reportListFlow.value = when(it){
+                    is HttpException -> EmpTaskRegState.Failure(Exception("${it.code()} - ${it.message()}"))
+                    else -> EmpTaskRegState.Failure(Exception("Error during getting reports: Check your connection"))
+                }
+            }
+        }.onFailure {
+            _reportListFlow.value = EmpTaskRegState.Failure(Exception("No token found. Please login first."))
+        }
+    }
+
+}
