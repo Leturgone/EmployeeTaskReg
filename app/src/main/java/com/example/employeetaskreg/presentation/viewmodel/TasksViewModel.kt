200a74447701022ad5e997331c665cf228ff191d
Leturgone <finekon221@gmail.com>
2025-05-18 23:51:29 +0300

    added method for adding task

--- app/src/main/java/com/example/employeetaskreg/presentation/viewmodel/TasksViewModel.kt
+++ app/src/main/java/com/example/employeetaskreg/presentation/viewmodel/TasksViewModel.kt
@@ -2,6 +2,7 @@ package com.example.employeetaskreg.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.employeetaskreg.data.api.dto.AddTaskRequest
 import com.example.employeetaskreg.domain.model.Task
 import com.example.employeetaskreg.domain.repository.AuthRepository
 import com.example.employeetaskreg.domain.repository.EmpTaskRegState
@@ -23,6 +24,11 @@ class TasksViewModel @Inject constructor(
     private val _taskListFlow = MutableStateFlow<EmpTaskRegState<List<Task>>>(EmpTaskRegState.Waiting)
 
     val taskListFlow: StateFlow<EmpTaskRegState<List<Task>>> = _taskListFlow
+
+    private val _addTaskFlow = MutableStateFlow<EmpTaskRegState<Unit>>(EmpTaskRegState.Waiting)
+    val addTaskFlow: StateFlow<EmpTaskRegState<Unit>> = _addTaskFlow
+
+
 
     fun getTaskList() = viewModelScope.launch {
         _taskListFlow.value = EmpTaskRegState.Loading
@@ -47,4 +53,32 @@ class TasksViewModel @Inject constructor(
             _taskListFlow.value = EmpTaskRegState.Failure(Exception("No token found. Please login first."))
         }
     }
+
+    fun addTask(title:String, taskDesc:String, documentName:String?,
+                startDate:String, endDate:String,
+                employeeId:Int, directorId:Int,filePath:String?) = viewModelScope.launch{
+        _addTaskFlow.value = EmpTaskRegState.Loading
+
+        val authResult = withContext(Dispatchers.IO){
+            authRepository.getTokenFromDataStorage()
+        }
+        authResult.onSuccess {token ->
+            val result = withContext(Dispatchers.IO){
+                val task = AddTaskRequest(title, taskDesc, documentName,startDate, endDate, employeeId, directorId)
+                taskRepository.addTask(task,filePath,token)
+            }
+            result.onSuccess {
+                _addTaskFlow.value = EmpTaskRegState.Success(it)
+            }.onFailure {
+                _addTaskFlow.value = when(it){
+                    is HttpException -> EmpTaskRegState.Failure(Exception("${it.code()} - ${it.message()}"))
+                    else -> EmpTaskRegState.Failure(Exception("Error during adding task: Check your connection"))
+                }
+            }
+
+        }.onFailure {
+            _addTaskFlow.value = EmpTaskRegState.Failure(Exception("No token found. Please login first."))
+        }
+    }
+
 }
