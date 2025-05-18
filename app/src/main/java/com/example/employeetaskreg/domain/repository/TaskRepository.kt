c3d823fb7f267c2abbfd2428bc894aea8e6a6a52
Leturgone <finekon221@gmail.com>
2025-05-18 23:51:14 +0300

    added method for adding task

--- app/src/main/java/com/example/employeetaskreg/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/example/employeetaskreg/domain/repository/TaskRepository.kt
@@ -1,5 +1,6 @@
 package com.example.employeetaskreg.domain.repository
 
+import com.example.employeetaskreg.data.api.dto.AddTaskRequest
 import com.example.employeetaskreg.domain.model.Task
 
 interface TaskRepository {
@@ -9,5 +10,7 @@ interface TaskRepository {
     suspend fun getTaskList(authToken:String):Result<List<Task>>
 
     suspend fun getEmployeeTaskCount(id:Int,authToken:String):Result<Int>
+
+    suspend fun addTask(task:AddTaskRequest,filePath: String?,authToken:String):Result<Unit>
 
 }
