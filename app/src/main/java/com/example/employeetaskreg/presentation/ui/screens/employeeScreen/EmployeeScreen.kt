919eea56f90cbf440ca5b3650d2c4d8c30dcba34
Leturgone <finekon221@gmail.com>
2025-05-21 01:01:51 +0300

    added load of employeeList

--- app/src/main/java/com/example/employeetaskreg/presentation/ui/screens/employeeScreen/EmployeeScreen.kt
+++ app/src/main/java/com/example/employeetaskreg/presentation/ui/screens/employeeScreen/EmployeeScreen.kt
@@ -1,51 +1,97 @@
 package com.example.employeetaskreg.presentation.ui.screens.employeeScreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.employeetaskreg.R
-@Preview
+import com.example.employeetaskreg.domain.model.CompanyWorker
+import com.example.employeetaskreg.domain.repository.EmpTaskRegState
+import com.example.employeetaskreg.presentation.ui.screens.CustomToastMessage
+import com.example.employeetaskreg.presentation.viewmodel.EmployeesViewModel
+
 @Composable
-fun EmployeesScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = stringResource(id = R.string.employees),
-            style = MaterialTheme.typography.headlineSmall,
-            fontWeight = FontWeight.Bold,
-            fontSize = 25.sp,
-            modifier = Modifier.padding(bottom = 16.dp, top = 16.dp)
+fun EmployeesScreen(viewModel: EmployeesViewModel = hiltViewModel()) {
+
+    val employeeListState = viewModel.employeesListFlow.collectAsState()
+
+    var showToast by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+    var employeeList by remember { mutableStateOf(emptyList<CompanyWorker.Employee>()) }
+
+
+    LaunchedEffect(Unit){
+        viewModel.getEmployeesList()
+    }
+
+    Box(){
+        CustomToastMessage(
+            message = errorMessage,
+            isVisible = showToast,
+            onDismiss = { showToast = false },
         )
 
-        SearchSec()
-
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            items(3) {
-                Spacer(modifier = Modifier.height(30.dp))
-                //EmployeeCard(name = "Иванов И.И.")
+            Text(
+                text = stringResource(id = R.string.employees),
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                modifier = Modifier.padding(bottom = 16.dp, top = 16.dp)
+            )
 
+            SearchSec()
+            when(employeeListState.value){
+                is EmpTaskRegState.Failure -> LaunchedEffect(employeeListState.value) {
+                    showToast = true
+                    errorMessage =
+                        (employeeListState.value as EmpTaskRegState.Failure).exception.toString()
+                }
+                EmpTaskRegState.Loading -> CircularProgressIndicator()
+                is EmpTaskRegState.Success -> employeeList = (employeeListState.value as EmpTaskRegState.Success<List<CompanyWorker.Employee>>).result
+                EmpTaskRegState.Waiting -> null
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(employeeList.size) {
+                    val employee = employeeList[it]
+                    Spacer(modifier = Modifier.height(30.dp))
+                    EmployeeCard(
+                        employeeName = employee.name,
+                        employeeId = employee.id,
+                        viewModel = viewModel
+                    )
+                }
             }
         }
     }
+
 }
 
 
