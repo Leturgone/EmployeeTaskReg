7c8fa176ebda2da4cc7ea19d26680e9d25b0d6ab
Leturgone <finekon221@gmail.com>
2025-05-24 13:29:14 +0300

    implemented byteArrayToFileMethod to file repository

--- app/src/main/java/com/example/employeetaskreg/data/repsitory/FileRepositoryImpl.kt
+++ app/src/main/java/com/example/employeetaskreg/data/repsitory/FileRepositoryImpl.kt
@@ -6,10 +6,11 @@ import android.util.Log
 import com.example.employeetaskreg.domain.repository.FileRepository
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.io.InputStream
 
 class FileRepositoryImpl(): FileRepository {
-    override fun uriToFile(context: Context, uri: Uri): File? {
+    override suspend fun uriToFile(context: Context, uri: Uri): File? {
         val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
         val file: File? = createTempFile(context)
         return try {
@@ -34,11 +35,26 @@ class FileRepositoryImpl(): FileRepository {
         }
     }
 
+    override suspend fun byteArrayToFile(context: Context, byteArray: ByteArray, destinationFile: File): Result<File> {
+        return try {
+            val outputStream = FileOutputStream(destinationFile)
+
+            outputStream.use { outputStream ->
+                outputStream.write(byteArray)
+                outputStream.flush()
+                Result.success(destinationFile)
+            }
+        }catch (e: IOException) {
+            Log.e("FileRepository", "Error saving ByteArray to file: ${e.message}")
+            Result.failure(e)
+        }
+    }
+
     private fun createTempFile(context: Context): File? {
         val storageDir: File? = context.cacheDir
         return try {
             File.createTempFile("pdf_", ".pdf", storageDir)
-        }catch (e:java.io.IOException){
+        }catch (e: IOException){
             null
         }
     }
